021e8780a5b5d6c724275ddcbc12a26de8927357
MeteoMartini <75040651+MeteoMartini@users.noreply.github.com>
2026-09-17 19:55:33 +0200

    Load C12 forecast redesign after Today workspace

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import './midC7Composition.css';
 import './midC8VisibleRedesign.css';
 import './midC10CurrentRedesign.css';
 import './midC11TodayRedesign.css';
+import './midC12ForecastRedesign.css';
 import './v078';
 import App from './App';
 import {restorePersistentState,startPersistenceBridge} from './persistence';
