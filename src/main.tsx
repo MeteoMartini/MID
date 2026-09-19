06f5b59fe9bc4c7dff6f29434e27a1184e62708f
MeteoMartini <75040651+MeteoMartini@users.noreply.github.com>
2026-09-19 22:28:57 +0200

    MID 18: Hierarchie-Layer zuletzt laden

--- src/main.tsx
+++ src/main.tsx
@@ -25,6 +25,7 @@ import './midDesign201MobilePolish.css';
 import './midDesign201MobileQa.css';
 import './midDesign201CurrentCritical.css';
 import './midC9SmartphoneGate.css';
+import './midC18HierarchyRedesign.css';
 import App from './App';
 import {restorePersistentState,startPersistenceBridge} from './persistence';
 import {getMidUpdateStatus,markMidRuntimeHealthy,registerMidServiceWorker,rollbackPendingMidUpdate} from './pwa';
