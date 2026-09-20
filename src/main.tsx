e48a9eab9a81a3cc3815a36b4f4ddfba696a7b12
MeteoMartini <75040651+MeteoMartini@users.noreply.github.com>
2026-09-20 08:48:50 +0200

    MID 18.1: Control-Grammar zuletzt laden

--- src/main.tsx
+++ src/main.tsx
@@ -30,6 +30,7 @@ import './midC18HierarchyPolish.css';
 import './midC18TypographyReadability.css';
 import './midC18SecondarySurfaceRefinement.css';
 import './midC18ShellMobileFinish.css';
+import './midC181ControlGrammar.css';
 import App from './App';
 import {restorePersistentState,startPersistenceBridge} from './persistence';
 import {getMidUpdateStatus,markMidRuntimeHealthy,registerMidServiceWorker,rollbackPendingMidUpdate} from './pwa';
