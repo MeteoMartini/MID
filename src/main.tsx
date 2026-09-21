dfcba64327c597004ffa4513f57ea8232633096f
MeteoMartini <75040651+MeteoMartini@users.noreply.github.com>
2026-09-21 06:22:11 +0200

    MID 18.2: load ensemble interaction polish last

--- src/main.tsx
+++ src/main.tsx
@@ -38,6 +38,7 @@ import './midC18FooterRhythmAudit.css';
 import './midC18UnifiedThreadTimeline.css';
 import './midC18InteractionCurrentPolish.css';
 import './midC18ForecastMapShellPolish.css';
+import './midC18EnsembleInteractionPolish.css';
 import './midC18SecondaryWorkspaceFinish.css';
 import './midC18TodayReleaseFinish.css';
 import App from './App';
